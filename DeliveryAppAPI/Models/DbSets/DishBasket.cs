8ee6d14a3f3ff893d4ba18ebe2815d9a66893c37
feofilovaleksey <Alexfil888@gmail.com>
2022-11-28 18:40:39 +0700

    Task #15340 - set DishBasket User nullable

--- DeliveryAppAPI/Models/DbSets/DishBasket.cs
+++ DeliveryAppAPI/Models/DbSets/DishBasket.cs
@@ -8,6 +8,6 @@ public class DishBasket
     [MinLength(1)]
     [Range(0, int.MaxValue)]
     public int Amount { get; set; }
-    public User User { get; set; }
+    public User? User { get; set; }
     public Dish Dish { get; set; }
 }
